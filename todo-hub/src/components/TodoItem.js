77688891e9d0fcbb3137f666ca9ddcde5b9da4b3
nandini <nininandini@gmail.com>
2024-04-18 11:45:56 -0700

    Add some packages.

--- todo-hub/src/components/TodoItem.js
+++ todo-hub/src/components/TodoItem.js
@@ -1,11 +1,25 @@
 import React from "react";
 
 const Todoitem=(Props)=>{
-    return( <div className="todo-item">
-       <input type="checkbox" checked={Props.check} />
-       <p>{Props.text}</p>
-       <button>Delete</button>
-    </div>)
+    return(<table className="todo-item">
+ <tbody>
+    <tr>
+      <input className="Taskinp" type="text" value="ggtf" /> 
+      <td className="actions">
+      <button className="btn">Edit </button> 
+      <button className="btn" >Delete</button>
+      </td>
+    </tr>
+  </tbody>
+  </table>
+
+
+
+
+    //    <input type="checkbox" checked={Props.check} />
+    //    <input type="text">{Props.text}</input>
+      
+   )
 }
 
 export default Todoitem;
--- todo-hub/src/components/TodoList.js
+++ todo-hub/src/components/TodoList.js
@@ -1,10 +1,19 @@
-import React  from "react";
+import React  ,{useState} from "react";
 import Todoitem from "./TodoItem";
 
 const Todolist=()=>{
+
+    const [task, setTask] = useState("");
+   // const [tasks, setTasks] = useState([]);
+
+    const AddTaskHandler=(e)=>{
+    //  setTask(e);
+    }
+
     return(<div className="todo-list">
-        <h1>TODO LIST</h1>      
-        <button className="btnadd">Add Task</button>
+        <h1>TODO LIST</h1>  
+        <input  className="Taskinp" type="text" placeholder="Enter a Task "/>    
+        <button className="btn" onChange={AddTaskHandler()}>Add Task</button>
         <div className="todoitems">
         <Todoitem text="create a app"/>
         <Todoitem text="cook for prisha"/>
